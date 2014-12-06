621eca02abe39190cbb9d4b28afac7050d4b9023
Шурик <alex@Okinawa-Mac-mini-2.local>
2014-12-06 23:45:02 +0200

    Sliced code

---
+++ DMManagerExample/Pods/Headers/Public/DMManager/DMManager.h
@@ -0,0 +1,1 @@
+../../../../../Classes/DMManager.h
---
+++ DMManagerExample/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DMManager
+#define COCOAPODS_POD_AVAILABLE_DMManager
+#define COCOAPODS_VERSION_MAJOR_DMManager 0
+#define COCOAPODS_VERSION_MINOR_DMManager 0
+#define COCOAPODS_VERSION_PATCH_DMManager 1
+
